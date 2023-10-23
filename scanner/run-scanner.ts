0358f8a31f1ecc6eb740d6da86e910364cc9a4f2
depidsvy <spidgorny@gmail.com>
2023-10-23 23:07:51 +0300

    - scanner

--- scanner/run-scanner.ts
+++ scanner/run-scanner.ts
@@ -3,11 +3,12 @@ import * as fs from "fs";
 import {Dirent} from "fs";
 import {db} from "@/server/database/database";
 import {Folder} from "@/server/database/types";
+import path from "node:path";
 
 runTest(async () => {
-    const root = '..';
-    const parent = null;
-    await scanDir(root, (dirent) => importCode(parent, dirent))
+    const root = path.resolve('..');
+    const parent = await findInsertFolderInDb(root);
+    await scanDir(root, (dirent: Dirent) => importCode(parent, dirent))
 }).then()
 
 async function scanDir(root: string, importCode: (file: Dirent)=>void) {
@@ -22,22 +23,14 @@ async function scanDir(root: string, importCode: (file: Dirent)=>void) {
 }
 
 async function importCode(parent: Folder, dirent: Dirent) {
-    let stat = fs.statSync(dirent.path);
     if (dirent.isDirectory()) {
-        let set = {
-            parent: parent?.id ?? null,
-            name: dirent.name,
-            size: stat.size,
-            ctime: stat.ctime,
-            mtime: stat.mtime,
-        };
-        const res = await db.insertInto('folder')
-            .values(set)
-            .onConflict(oc => oc
-            .columns(['parent', 'name'])
-            .doUpdateSet(set)).execute();
-        return;
+        return insertUpdateDirectory(parent, dirent);
     }
+    return insertUpdateFile(parent, dirent);
+}
+
+async function insertUpdateFile(parent: Folder, dirent: Dirent) {
+    let stat = fs.statSync(dirent.path);
     let set = {
         id_folder: parent?.id ?? null,
         name: dirent.name,
@@ -45,8 +38,90 @@ async function importCode(parent: Folder, dirent: Dirent) {
         ctime: stat.ctime,
         mtime: stat.mtime,
     };
-    const res = await db.insertInto('file').values(set)
-        .onConflict(oc => oc
-            .columns(['id_folder', 'name'])
-            .doUpdateSet(set)).execute();
+    if (parent) {
+        const res = await db.insertInto('file').values({
+            ...set,
+            created_at: new Date(),
+        })
+            .onConflict(oc => oc
+                .columns(['id_folder', 'name'])
+                .doUpdateSet({
+                    ...set,
+                    updated_at: new Date(),
+                })).execute();
+        return res;
+    }
+
+    const row = await db.selectFrom('file').selectAll().where('name', '=', dirent.name).executeTakeFirst();
+    if (row) {
+        const res = await db.updateTable('file').set({
+            ...set,
+            created_at: new Date(),
+        }).where('name', '=', dirent.name).execute();
+        return res;
+    } else {
+        const res = await db.insertInto('file').values({
+            ...set,
+            created_at: new Date(),
+        }).execute();
+return res;
+    }
+}
+
+async function insertUpdateDirectory(parent: Folder, dirent: Dirent) {
+    let stat = fs.statSync(dirent.path);
+    let set = {
+        parent: parent?.id ?? null,
+        name: dirent.name,
+        size: stat.size,
+        ctime: stat.ctime,
+        mtime: stat.mtime,
+    };
+    if (!parent) {
+        throw new Error('No parent directory when inserting new folder');
+    }
+        const res = await db.insertInto('folder')
+            .values({
+                ...set,
+                created_at: new Date(),
+            })
+            .onConflict(oc => oc
+                .columns(['parent', 'name'])
+                .doUpdateSet({
+                    ...set,
+                    updated_at: new Date(),
+                })).execute();
+        return res;
+
+
+}
+
+async function findFolderByName(dirent: Dirent) {
+    const row = await db.selectFrom('folder').selectAll().where('name', '=', dirent.name).executeTakeFirst();
+    return row;
+}
+
+async function findInsertFolderInDb(dirent: Dirent) {
+    const row = findFolderByName(dirent);
+    if (row) {
+        return row;
+    }
+    return insertRootDirectory(dirent);
+}
+
+async function insertRootDirectory(dirent: Dirent) {
+    let stat = fs.statSync(dirent.path);
+    let set = {
+        parent: parent?.id ?? null,
+        name: dirent.name,
+        size: stat.size,
+        ctime: stat.ctime,
+        mtime: stat.mtime,
+    };
+        const res = await db.insertInto('folder').values({
+            ...set,
+            created_at: new Date(),
+        }).execute();
+        return res;
+
 }

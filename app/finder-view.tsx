44f7d017dbd51ef2cc7f231c52c9e922f6d2e452
depidsvy <spidgorny@gmail.com>
2023-10-24 13:15:29 +0300

    - finder does not use client

---
+++ app/finder-view.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import "finderjs/example/finderjs.css";
+import {ReactFinder} from "./react-finder";
+
+export default function FinderView() {
+    const data = [
+        {
+            id: 1,
+            label: "Label A",
+            children: [
+                {
+                    id: 10,
+                    label: "Label A1",
+                },
+                {
+                    id: 11,
+                    label: "Label A2",
+                },
+            ],
+        },
+        {
+            id: 2,
+            label: "Label B",
+        },
+    ];
+
+    return (
+        <main className="p-3 bg-white">
+            <ReactFinder data={data}/>
+        </main>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,11 @@
-"use client";
-
 import "finderjs/example/finderjs.css";
-import { ReactFinder } from "./react-finder";
+import {ReactFinder} from "./react-finder";
+import FinderView from "@/app/finder-view";
 
 export default function Home() {
-	const data = [
-		{
-			id: 1,
-			label: "Label A",
-			children: [
-				{
-					id: 10,
-					label: "Label A1",
-				},
-				{
-					id: 11,
-					label: "Label A2",
-				},
-			],
-		},
-		{
-			id: 2,
-			label: "Label B",
-		},
-	];
-
-	return (
-		<main className="p-3 bg-white">
-			<ReactFinder className="" data={data} />
-		</main>
-	);
+    return (
+        <main className="p-3 bg-white">
+            <FinderView/>
+        </main>
+    );
 }
